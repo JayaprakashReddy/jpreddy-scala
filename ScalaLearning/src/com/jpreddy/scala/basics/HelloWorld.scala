42ef759d3621cc9a6715b097e99b349dd747527e
JayaprakashReddy <jayaprakashreddy@gmail.com>
2014-09-03 20:34:43 +1000

    My Scala project creation

---
+++ ScalaLearning/src/com/jpreddy/scala/basics/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.jpreddy.scala.basics
+
+object HelloWorld {
+	def main(args: Array[String]) {
+		println("Hello, world!")
+		myfunc()
+	}
+	def myfunc()
+	{
+	  println("This is JP")
+	}
+}
